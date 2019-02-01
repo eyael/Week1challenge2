5efa54fa64e294e54dae7916e33c5ad7be790725
eyael <eyaelja1@gmail.com>
2019-02-01 16:58:52 -0500

    did it

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+
+import java.util.Random;
+
+    public class Main {
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int money = 0;
+            int randomm = 0;
+
+            System.out.println("WELCOME! Let's play some Roulette!");
+
+
+                System.out.print("Enter the amount of money" + "type of bet you would like to place (red|black|even|odd|high|low + between 1 and 36");
+
+                 int n = rand.nextInt(36) + 1;
+                double random = Math.random() * 35 + 1;
+                while (money!= random);
+            System.out.println("You Won");
+
+
+            }
+
+
+
+        }
+
+

a9a7d73d3b08c988c8277652587976d31c05e90a
eyaelja@gmail.com <eyaelja1@gmail.com>
2019-02-04 17:06:27 -0500

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,60 @@ package com.company;
 
 
 import java.util.Random;
+import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args) {
             Random rand = new Random();
-            int money = 0;
-            int randomm = 0;
+            int score = 0;
+            boolean answer;
 
+
+            Scanner keyboard = new Scanner(System.in);
             System.out.println("WELCOME! Let's play some Roulette!");
 
 
-                System.out.print("Enter the amount of money" + "type of bet you would like to place (red|black|even|odd|high|low + between 1 and 36");
+           do {
+               System.out.print("Enter the amount of money" + "type of bet you would like to place (even|odd|high|low");
+               String bet = keyboard.next();
 
-                 int n = rand.nextInt(36) + 1;
-                double random = Math.random() * 35 + 1;
-                while (money!= random);
-            System.out.println("You Won");
+               System.out.println("enter your bet amount");
+               int amount = keyboard.nextInt();
+
+               int index = 1 + rand.nextInt(36);
+               System.out.println("The ball landed on " + index);
+
+               if (index >= 1 && index <= 18 && bet.equalsIgnoreCase("low")) {
+                   score = score + (amount * 2);
+                   System.out.println("Congratulations, you've won");
+                   System.out.println("You currently have" + score);
+               } else if (index >= 19 && index <= 36 && bet.equalsIgnoreCase("high")) {
+                   score = score + (amount * 2);
+                   System.out.println("Congratulations, you've won");
+                   System.out.println("You currently have" + score);
+
+               } else if (index % 2 == 0 && bet.equalsIgnoreCase("even")) {
+                   score = score + (amount * 2);
+                   System.out.println("Congratulations, you've won");
+                   System.out.println("You currently have" + score);
+
+               } else if (index % 2 != 0 && bet.equalsIgnoreCase("odd")) {
+                   score = score + (amount * 2);
+                   System.out.println("Congratulations, you've won");
+                   System.out.println("You currently have" + score);
+               } else {
+                   score = score - (amount);
+                   System.out.println("Sorry, you've lost this bet");
+                   System.out.println("You currently have" + score);
+               }
+               System.out.println ("Would you like to play again (true|false)?");
+                answer = keyboard.nextBoolean();
+
+           }while (answer==true);
+            System.out.println("Thank you for playing!\n" +
+                    "This is how much money you have remaining"+ score);
+
+
 
 
             }
